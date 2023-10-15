46bfe2a2ab714c0599ab36339108b941af07056c
Edu Carmona <dudu.carmona@gmail.com>
2023-10-16 00:11:33 +0200

    Deploy con el código 'oficial'

--- app.js
+++ app.js
@@ -1,44 +1,55 @@
-const express = require('express') // require --> commonJS
+const express = require('express') // require -> commonJS
 const crypto = require('node:crypto')
+const cors = require('cors')
+
 const movies = require('./movies.json')
-const { validateMovie, validatePartialMovie } = require('./schemas/movies.js')
-const { error } = require('node:console')
+const { validateMovie, validatePartialMovie } = require('./schemas/movies')
 
 const app = express()
-
 app.use(express.json())
-app.disable('x-powered-by') // deshabilitar el header X-Powered by Express
+app.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:8080',
+      'http://localhost:1234',
+      'https://movies.com',
+      'https://midu.dev'
+    ]
 
-const ACCEPTED_ORIGINS = [
-  'http://localhost:8080',
-  'http://localhost:1234',
-  'https://movies.com',
-  'https://midu.dev'
-]
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true)
+    }
 
-// Todos los recursos que sean MOVIES se identifican con /movies
-app.get('/movies', (req, res) => {
-  const origin = req.header('origin')
+    if (!origin) {
+      return callback(null, true)
+    }
 
-  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
+    return callback(new Error('Not allowed by CORS'))
   }
+}))
+app.disable('x-powered-by') // deshabilitar el header X-Powered-By: Express
 
+// métodos normales: GET/HEAD/POST
+// métodos complejos: PUT/PATCH/DELETE
+
+// CORS PRE-Flight
+// OPTIONS
+
+// Todos los recursos que sean MOVIES se identifica con /movies
+app.get('/movies', (req, res) => {
   const { genre } = req.query
-
   if (genre) {
-    const filteredMovies = movies.filter(movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase()))
-
+    const filteredMovies = movies.filter(
+      movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+    )
     return res.json(filteredMovies)
   }
-
   res.json(movies)
 })
 
 app.get('/movies/:id', (req, res) => {
   const { id } = req.params
   const movie = movies.find(movie => movie.id === id)
-
   if (movie) return res.json(movie)
   res.status(404).json({ message: 'Movie not found' })
 })
@@ -51,26 +62,20 @@ app.post('/movies', (req, res) => {
     return res.status(400).json({ error: JSON.parse(result.error.message) })
   }
 
-  // En base de datos
+  // en base de datos
   const newMovie = {
-    id: crypto.randomUUID(),
+    id: crypto.randomUUID(), // uuid v4
     ...result.data
   }
 
-  // Esto no sería REST porque estamos guardando
+  // Esto no sería REST, porque estamos guardando
   // el estado de la aplicación en memoria
   movies.push(newMovie)
 
-  res.status(201).json(newMovie) // actualizar la caché del cliente
+  res.status(201).json(newMovie)
 })
 
 app.delete('/movies/:id', (req, res) => {
-  const origin = req.header('origin')
-
-  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
-  }
-
   const { id } = req.params
   const movieIndex = movies.findIndex(movie => movie.id === id)
 
@@ -87,34 +92,28 @@ app.patch('/movies/:id', (req, res) => {
   const result = validatePartialMovie(req.body)
 
   if (!result.success) {
-    return res.status(400).json({ error: JSON.parse(error.result.message) })
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
   }
 
   const { id } = req.params
   const movieIndex = movies.findIndex(movie => movie.id === id)
 
   if (movieIndex === -1) {
-    return res.status(404).json({ message: 'Movie not found.' })
+    return res.status(404).json({ message: 'Movie not found' })
   }
 
-  const updatedMovie = { ...movies[movieIndex], ...result.data }
-
-  movies[movieIndex] = updatedMovie
-
-  return res.json(updatedMovie)
-})
-
-app.options('/movies/:id', (req, res) => {
-  const origin = req.header('origin')
-
-  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data
   }
 
-  res.send(200)
+  movies[movieIndex] = updateMovie
+
+  return res.json(updateMovie)
 })
 
 const PORT = process.env.PORT ?? 1234
 
-app.listen(PORT, () => console.log(`server listening on port http://localhost:${PORT}`))
+app.listen(PORT, () => {
+  console.log(`server listening on port http://localhost:${PORT}`)
+})
--- schemas/movies.js
+++ schemas/movies.js
@@ -2,7 +2,7 @@ const z = require('zod')
 
 const movieSchema = z.object({
   title: z.string({
-    invalid_type_error: 'Movie title must be a string.',
+    invalid_type_error: 'Movie title must be a string',
     required_error: 'Movie title is required.'
   }),
   year: z.number().int().min(1900).max(2024),
@@ -10,13 +10,13 @@ const movieSchema = z.object({
   duration: z.number().int().positive(),
   rate: z.number().min(0).max(10).default(5),
   poster: z.string().url({
-    message: 'Poster must be a valid URL.'
+    message: 'Poster must be a valid URL'
   }),
   genre: z.array(
-    z.enum(['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi', 'Crime', 'Romance']),
+    z.enum(['Action', 'Adventure', 'Crime', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi']),
     {
       required_error: 'Movie genre is required.',
-      invalid_type_error: 'Movie genre must be an array of enum Genre.'
+      invalid_type_error: 'Movie genre must be an array of enum Genre'
     }
   )
 })
